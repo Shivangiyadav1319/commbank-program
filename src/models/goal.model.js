f71d9e3936857a297f4341765668d39fbd24df29
Shivangiyadav1319 <140613007+Shivangiyadav1319@users.noreply.github.com>
2026-02-12 14:26:54 +0530

    Create src/models/goal.model.js

---
+++ src/models/goal.model.js
@@ -0,0 +1,13 @@
+// Frontend Goal model (client-side)
+
+export const Goal = {
+  id: "1234",
+  name: "abcd",
+  targetAmount: 100000,
+  targetDate: "01 jan - 01 dec",
+  balance: 0,
+  transactionIds: [0234],
+  tagIds: [234],
+  icon: "", // optional emoji/icon
+  userId: ""
+};
